d17569a2800396f483e05cca5adb44f3017dd7ea
tuana9a <tuana9a@gmail.com>
2021-09-11 14:47:58 +0700

    remove docker and update README

--- README.md
+++ README.md
@@ -3,18 +3,24 @@
 <pre>
 webservice nhận diện captcha các trang web trường đại học Bách Khoa Hà Nội thành text
 mình dùng thư viện <a href="https://github.com/pbcquoc/vietocr">pbcquoc/vietocr</a> đã train tiếp models với ~2000 bức ảnh
-các bạn có thể <a href="https://github.com/tuana9a/tuana9a">liên hệ mình</a> để lấy trọng số, config, ảnh đã dán nhãn hoặc cần hỗ trợ
+các bạn có thể <a href="https://github.com/tuana9a/tuana9a">liên hệ mình</a> nếu cần hỗ trợ
+mọi thông tin có thể public mình để ở <a href="https://drive.google.com/drive/folders/1YAVIaEI7ffRzCIAhXJNJYV_bVdQLie4F?usp=sharing">thư mục drive</a>
 </pre>
 
 # #requirements
 
-<strong><i style="color:red">CAUTION:</i> vietocr use pyyaml</strong>
+<strong><b style="color:red">NOTE:</b>
+<br>
+pip install -r requirements.txt nếu có lỗi thì bạn có thể cài thủ công phía dưới
+<br>
+pytorch mình cài là CPU, với CUDA hiện tại mình chưa test, các bạn có thể cài và thử từ <a href="https://pytorch.org/">trang chủ</a> pytorch
+</strong>
 
 <pre>
 pip install Flask==2.0.1
-pip install Pillow==8.2.0
+pip install Pillow==8.3.2
 pip install pyyaml==5.4.1
-pip install torch==1.7.1+cpu torchvision==0.8.2+cpu torchaudio===0.7.2 -f https://download.pytorch.org/whl/torch_stable.html
+pip install torch==1.8.2+cpu torchvision==0.9.2+cpu torchaudio===0.8.2 -f https://download.pytorch.org/whl/lts/1.8/torch_lts.html
 pip install vietocr==0.3.2
 </pre>
 
@@ -24,9 +30,9 @@ pip install vietocr==0.3.2
 -- ./
    |-- .venv/
    |-- resource/
-   |       |-- app-config.yml
+   |       |-- app-config.yaml
    |       |-- weights.pth
-   |       |-- weights-config.yml
+   |       |-- weights-config.yaml
    |-- main.py
 </pre>
 
--- main.py
+++ main.py
@@ -5,30 +5,29 @@ import yaml
 import logging
 
 from PIL import Image
-from flask import Flask, request, flash
+from flask import Flask, request
 from werkzeug.utils import secure_filename
 
-from datetime import datetime
 from vietocr.tool.config import Cfg
 from vietocr.tool.predictor import Predictor
 
 
-with open('resource/app-config.yml') as f:
+with open("resource/app-config.yaml") as f:
     AppConfig = yaml.load(f, Loader=yaml.FullLoader)
 
 
 def load_model():
-    config = AppConfig['vietocr']
-    model_config = Cfg.load_config_from_file(config['weights']['config'])
-    model_config['weights'] = config['weights']['path']
-    model_config['device'] = config['device_type']
+    config = AppConfig["vietocr"]
+    model_config = Cfg.load_config_from_file(config["weights"]["config"])
+    model_config["weights"] = config["weights"]["path"]
+    model_config["device"] = config["device_type"]
     # EXPLAIN: để false vì mình không train
-    model_config['cnn']['pretrained'] = False
+    model_config["cnn"]["pretrained"] = False
     model = Predictor(model_config)
     return model
 
 
-def init_server(custom_config=None):
+def create_server(custom_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
 
@@ -45,60 +44,67 @@ def init_server(custom_config=None):
 
     # manual config
     app.config.from_mapping(
-        SECRET_KEY='b7HHex1dxdfjxfcxd3x1b!xb4xe6m',
+        SECRET_KEY="b7HHex1dxdfjxfcxd3x1b!xb4xe6m",
         MAX_CONTENT_LENGTH=5 * 1024 * 1024,  # EXPLAIN: 5MB
     )
 
     # custom logging
-    log = logging.getLogger('werkzeug')
+    log = logging.getLogger("werkzeug")
     log.setLevel(logging.ERROR)
     return app
 
 
-def check_allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in AppConfig['flask']['allowed_extension']
+def is_allowed_extension(filename):
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in AppConfig["flask"]["allowed_extension"]
 
 
 def main():
     model = load_model()
-    app = init_server()
+    server = create_server()
 
-    @app.route('/api/captcha/to/text', methods=['POST'])
+    @server.route("/", methods=["GET"])
+    def index():
+        return "hello world"
+
+    @server.route("/api/captcha/to/text", methods=["GET"])
+    def _captcha_to_text():
+        return "your are right"
+
+    @server.route("/api/captcha/to/text", methods=["POST"])
     def captcha_to_text():
-        if 'file' not in request.files:
-            flash('No file part')
-            return 'no file upload'
+        if "file" not in request.files:
+            return "no file upload"
 
-        uploadfile = request.files['file']
+        uploadfile = request.files["file"]
+
+        if not uploadfile:
+            return "no file upload"
+
         filename = secure_filename(uploadfile.filename)
 
-        if uploadfile and check_allowed_file(filename):
-            try:
-                buffers = uploadfile.stream._file.getvalue()
-                image = Image.open(io.BytesIO(buffers))
-                result = model.predict(img=image)
-                return result
+        if not is_allowed_extension(filename):
+            return "not allowed " + filename
 
-            except:
-                now = datetime.now().strftime("%Y-%m-%d")
-                log_path = AppConfig['path']['logs_dir'] + f'{now}.log'
-                logging.basicConfig(filename=log_path, filemode='a',
-                                    format='%(asctime)s [%(levelname)s] %(message)s')
-                logging.error(f' file: {uploadfile}')
+        try:
+            buffers = uploadfile.stream._file.getvalue()
+            image = Image.open(io.BytesIO(buffers))
+            result = model.predict(img=image)
+            return result
 
-                return 'Error'
+        except:
+            return "I don't know"
 
-        return 'not allowed: ' + filename
-
-    print(' * Listen: ' + AppConfig['server']['address'])
-    app.run(host=AppConfig['server']['bind'], port=AppConfig['server']['port'])
+    bind = AppConfig["server"]["bind"]
+    port = os.environ.get("PORT") or AppConfig["server"]["port"]
+    print(" * Listen: " + str(port))
+    server.run(host=bind, port=port)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
-        print('KeyboardInterupt')
+        print("KeyboardInterupt")
         try:
             sys.exit(0)
         except SystemExit:
